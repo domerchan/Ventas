31c018ad80334672ba4d316f77e678c901bc158a
domerchan <domerchan@users.noreply.github.com>
2019-04-17 02:02:32 -0500

    index mas formato css, javascript para el nav (efecto sticky)

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var altura = $('.navHeader').offset().top;
+
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > altura) {
+			$('.navHeader').addClass('navFixed');
+		} else {
+			$('.navHeader').removeClass('navFixed');
+		}
+	});
+});
