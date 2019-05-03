2dd8ee0f2c6e698e1eb84bdc2bf4265389dc540e
domerchan <domerchan@users.noreply.github.com>
2019-05-03 15:37:21 -0500

    promociones en el index

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,6 @@ var marketControllers = angular.module('marketControllers', []);
 //Inyectamos el servicio Product creaado en services JS, que nos permite extraer los productos del proyecto
 marketControllers.controller('ProductListCtrl', ['$scope', 'Product',
   	function($scope, Product) {
-	/* Agregamos nuestros primeros carros <3 */
       $scope.products = Product.getProducts();
   }]);
 
