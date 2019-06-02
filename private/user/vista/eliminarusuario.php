b2f64c5840ec1b5a3af8cc40675f37a2a8a5f19c
jhtc5898 <33676340+jhtc5898@users.noreply.github.com>
2019-06-01 20:39:56 -0500

    Eliminar Usuario Cambios

---
+++ private/user/vista/eliminarusuario.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged']==false){
+        header("Location: /SistemaDeGestion/public/vista/login.html");
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        include '../../../config/conexionBD.php';
+        $codigo=$_SESSION['us_codigo'];
+        $sql="UPDATE usuario SET us_eliminado='S' WHERE us_codigo='$codigo'";                                            
+        if ($conn->query($sql) === TRUE) {
+            echo "<a>Datos  De La Cuenta Eliminados</a>";
+            header('Refresh: 5; URL=../../../config/cerrar_sesion.php');
+        }else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn) . "<br>";
+        }
+        $conn->close();
+        ?>
+</body>    
+</html>
